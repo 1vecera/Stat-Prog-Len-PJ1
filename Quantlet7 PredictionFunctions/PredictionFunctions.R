0ff31fa4cc55c9276c7158af72506a339f7f0682
1vecera <veceda@gmail.com>
2018-03-06 15:08:46 +0100

    Final Version of the Quantlet codes 7 8 and adding data to be self sufficient

--- Quantlet7 PredictionFunctions/PredictionFunctions.R
+++ Quantlet7 PredictionFunctions/PredictionFunctions.R
@@ -1,5 +1,3 @@
-
-
 # Ridge Regression Model --------------------------------------------------
 
 ridge_regression = function(formula, lambda = 0, data) {
@@ -8,14 +6,12 @@ ridge_regression = function(formula, lambda = 0, data) {
             ]]
   Beta = solve(t(X) %*% X +
                  diag(x = lambda, ncol = ncol(X), nrow = ncol(X))) %*% #αdd something to the diag of X'X
-    t(X) %*%  y
+                 t(X) %*%  y
   predictions = X %*% Beta #Predictions are just X times the beta coefficient
-  SSR= t((y- predictions)) %*% (y- predictions) #Sum of square residuals 
-  MSE = SSR/length(y) #Means square error
-  results = list(Beta = Beta, predictions = predictions, SSR = SSR, MSE = MSE  )
-  return(results)
-  
-}
+  SSR         = t((y- predictions)) %*% (y- predictions) #Sum of square residuals 
+  MSE         = SSR/length(y) #Means square error
+  results     = list(Beta = Beta, predictions = predictions, SSR = SSR, MSE = MSE  )
+  return(results)  }
 
 # #Funcitonto calculate the Percante correctly specified ------------------
 
@@ -24,16 +20,15 @@ calc_ppc = function(threshold_int, prob, truth) {
   #Sum the true positive and true negative 
   correctly_classified       =  sum(diag(
     prop.table(table(predictions_test_response,truth)))) 
-  return(correctly_classified)
-}
+  return(correctly_classified)}
 
 # Cross Validation --------------------------------------------------------
 #Function to return crossvalidation error, functions for any additive model with formula interface 
 
 cross_validation = function(fct, beta_location = "Beta", ntimes = 5, data, formula,...,
                             return_mean_PPC= T){
-  splits = sample(x = 1:ntimes, size = nrow(data), replace = T) #Creates indizes for number of splists
-  res_array = c()
+  splits          = sample(x = 1:ntimes, size = nrow(data), replace = T) #Creates indizes for number of splists
+  res_array       = c()
   thrashold_array = c()
   #Create test train split, perfrom the function, made predictions and calculate PPC
   for (i in 1:ntimes){
@@ -47,10 +42,8 @@ cross_validation = function(fct, beta_location = "Beta", ntimes = 5, data, formu
     opt_pcc          = optimise(calc_ppc,interval = c(0,1), maximum = T, #find ideal tharshold, and return associated PPC
                           tol = 0.01, prob = predictions_test, truth = y_test)
     res_array        = c(res_array, opt_pcc[[2]]) #Save the tharshold
-    thrashold_array  = c(thrashold_array, opt_pcc[[1]]) #Save the ppc
-  }
+    thrashold_array  = c(thrashold_array, opt_pcc[[1]]) }#Save the ppc  
   #For optimazations purposes, we want to get a single number with mean PPC
   #Other times, we want to get the list of the results
   if  (return_mean_PPC) res = mean(res_array) else res = list("PPC" = res_array, "Opt_TH" = thrashold_array)
-  return(res)
-}
+  return(res)}
--- Quantlet8 PredictionRidgeVsLogit/PredictionRidgeVsLogit.R
+++ Quantlet8 PredictionRidgeVsLogit/PredictionRidgeVsLogit.R
@@ -1,18 +1,17 @@
 #Analysis of the prediction power
-# library(RSQLite)
-# If you instead want to use the database.sqlite file, please connect it
-# Else use the Match and Data Players tables
-# con = dbConnect(SQLite(), dbname="database.sqlite")
-# Match = tbl_df(dbGetQuery(con,"SELECT * FROM Match"))
+  # library(RSQLite)
+  # If you instead want to use the database.sqlite file, please connect it
+  # Else use the Match and Data Players tables
+  # con = dbConnect(SQLite(), dbname="database.sqlite")
+  # Match = tbl_df(dbGetQuery(con,"SELECT * FROM Match"))
 
-library(dplyr)
 library(data.table)
 library(pROC)
 library(ggplot2)
 
-?grepl
-#First we need to make sure we have the right data
+# Data Reading ------------------------------------------------------------
 
+#First we need to make sure we have the right data
 Match = readRDS("Match.rds")
 Match = as.data.table(Match)
 
@@ -20,72 +19,80 @@ Match = as.data.table(Match)
 #Mostly only the player ids and other parts
 Match_filter = Match[,c(1,2,4,6,7,10,11,44,55,56:77)]
 Match_filter = na.omit(Match_filter) #Get rid of matches with missing values and player ides
+
 #We are talking about only 4000 rows
 #We decided to leave the first season out of our analysis
 Match_filter = Match_filter[season != "2008/2009"]
 
 #Now we can start joining in the results of the pca analysis
-
 Data_players = data.table(readRDS("Data_players_Match_predictions.rds"))
+
+# Data Joins --------------------------------------------------------------
 
 #We use the different columns to get the data together
 merge_list = list()
-
 for (i in names(Match_filter)[10:31]) {
-  #For each play in the match, we join the 3 PCA columns
-  merge_loop = merge(x = Data_players, y = Match_filter,by.x= "player_api_id", by.y = i, all.y = T)
-  merge_loop = merge_loop[order(match_api_id),] #Order everything in the same order
-  merge_loop  = merge_loop[,c(1:5,10)] #We want just the RC and the information about the players 
+  #For each play in the match, we join the 3 PCA columns, we use Data.Tables merge
+  merge_loop      = merge(x = Data_players, y = Match_filter,by.x= "player_api_id", by.y = i, all.y = T)
+  merge_loop      = merge_loop[order(match_api_id),] #Order everything in the same order
+  merge_loop      = merge_loop[,c(1:5,10)] #We want just the RC and the information about the players 
   merge_list[[i]] = merge_loop
 }
 merge_df_raw = do.call(cbind,merge_list)
 
-
 #get rid of columns with api_id as part of them after the controlling them manually 
-id_columns_location = grepl("api_id",names(merge_df_raw))
-merge_df_clean = merge_df_raw[,-id_columns_location,with = F]
+id_columns_location = grepl("api_id",names(merge_df_raw)) #Gets out the ID Columns names locatoin
+merge_df_clean      = merge_df_raw[,-id_columns_location,with = F]
+
 #Add the original columns from the match data
 Match_filter= Match_filter[order(match_api_id)] #make sure we don't missmatch the data
 if (identical(
   merge_df_raw$home_player_3.match_api_id,
-  Match_filter$match_api_id)){ #Sample check if the ids are right
+  Match_filter$match_api_id)){ #Sample check if the ids are synchhronized
   merge_df_clean = cbind(Match_filter, merge_df_clean )
   print("Columns added, please do not rerun.")}
 
 merge_df_clean = data.table(na.omit(merge_df_clean)) #Get rid of circa 1000 rows which are not complete
-#Add column showing if the home has won using the data.table syntax :=
 
-merge_df_clean[home_team_goal > away_team_goal, home_team_win:= T ]
+#Add column showing if the home has won using the data.table syntax :=
+merge_df_clean[home_team_goal  > away_team_goal, home_team_win:= T ]
 merge_df_clean[!home_team_goal > away_team_goal, home_team_win:= F ]
+
+# Creating Team Means -----------------------------------------------------
+
 #We wanted to see if the means themselves can be used as predictions factors instead the ones of the single players
-subset_RC_Columns = merge_df_clean[,grepl("RC_",names(merge_df_clean)),with = F]
+subset_RC_Columns      = merge_df_clean[,grepl("RC_",names(merge_df_clean)),with = F] #Only rotated copomnents
 subset_RC_Columns_home = subset_RC_Columns[,grepl("home_",names(subset_RC_Columns)),with = F]
 subset_RC_Columns_away = subset_RC_Columns[,!grepl("home_",names(subset_RC_Columns)),with = F]
 
-Principal_Components_Means_List = list()
+#Calculating the means of the components
+  Principal_Components_Means_List = list()
+  #Calculate means for all of the teams and components
+  for (i in paste0("4_",1:4)){ #Getting one after each other the columns out we requre
+    name_column_home = paste0("mean_home_",i) #creating names for the lists
+    name_column_away = paste0("mean_away_",i)
+    #Calculating the means for hte rows of the selected colums
+    Principal_Components_Means_List[[name_column_home]] = 
+      rowMeans(x = subset_RC_Columns_home[,grepl(i,names(subset_RC_Columns_home)),with = F])
+    Principal_Components_Means_List[[name_column_away]] = 
+      rowMeans(x = subset_RC_Columns_away[,grepl(i,names(subset_RC_Columns_away)),with = F])
+  }
 
-#Calculate means for all of the teams and components
-for (i in paste0("4_",1:4)){ #Getting one after each other the columns out we requre
-  name_column_home = paste0("mean_home_",i) #creating names for the lists
-  name_column_away = paste0("mean_away_",i)
-  
-  #Calculating the means for hte rows of the selected colums
-  Principal_Components_Means_List[[name_column_home]] = 
-    rowMeans(x = subset_RC_Columns_home[,grepl(i,names(subset_RC_Columns_home)),with = F])
-  Principal_Components_Means_List[[name_column_away]] = 
-    rowMeans(x = subset_RC_Columns_away[,grepl(i,names(subset_RC_Columns_away)),with = F])
-}
 #mean of the factors
 #Combining into the data frame
 Principal_Components_Means_DT = do.call(cbind.data.frame,Principal_Components_Means_List)
 #Sorting the data to have first home and than away columns
-Principal_Components_Means_DT = Principal_Components_Means_DT[,sort(names(Principal_Components_Means_DT),decreasing = T)]
-
+Principal_Components_Means_DT = 
+  Principal_Components_Means_DT[,sort(names(Principal_Components_Means_DT),decreasing = T)]
 
 #Join the final data together, want just the result, the prediction columns and id so we have something to check if it does make sense
 prediction_data_fin = data.table(merge_df_clean[,c(1,33:length(merge_df_clean)),with = F])
-#We add the means also to the prediction data
+#We add the also the home team win to the compoments means for easier regression aftewards
 Principal_Components_Means_DT$home_team_win = prediction_data_fin$home_team_win
+
+
+# Creation of interaction tersm -------------------------------------------
+
 #We want to test interaction terms for different teams
 #For each team and each principal component, we want to create the name
 interaction_terms = " home_team_win ~ . - id"  #We start by string which we used for the first regressiion
@@ -105,6 +112,8 @@ for (i in paste0("4_",1:4)){ #We go component by component and create the intera
 }
 
 
+# Logistic Regression -----------------------------------------------------
+
 #We perfrom logistical regression using the logit function
 reg_list = list()
 reg_list$reg_no_interact = glm(home_team_win ~ . - id,
@@ -123,14 +132,15 @@ for (i in 1:3)  fitted_values_list[[(names(reg_list)[i])]] =  reg_list[[i]][["fi
 #We use auc from pROC package
 sapply(X = fitted_values_list,FUN = auc, response = merge_df_clean$home_team_win) 
 #We need the functions of the quantlet 7
-source("Quantlet7_Prediction_Functions/Quantlet7_Prediction_Functions.R")
+source("Quantlet7 PredictionFunctions/PredictionFunctions.R")
 #Proving that thrashold optimazation does not bring a lot of value with logit models
 thrashold_opt  = sapply(X = 0:100/101,FUN = calc_ppc,
                         prob = reg_list$reg_no_interact$fitted.values, truth = prediction_data_fin$home_team_win )
 qplot(0:100/101, thrashold_opt, geom = "line" ) + theme_bw() +
   xlab("Thrashold") + ylab("Percantage correctly Classfied")
 
-###Applying the ridge ression to our other problem
+
+# Ridge Regression --------------------------------------------------------
 #Use the ridge function to perform naive prediction model
 
 #We search for optimal lamda
@@ -152,18 +162,20 @@ qplot(lambdas,pcc_array) + theme_bw() + xlab("Lambda") + ylab("Percantage Correc
 #We create search for the best lambda
 set.seed(123)
 opt_lam = optimise(f = cross_validation,fct = ridge_regression, formula = home_team_win ~ . - id,
-                           ntimes = 3, data = prediction_data_fin,
-                 beta_location = "Beta", tol = 0.001, interval = c(0,40000), return_mean_PPC = T,
-                 maximum = T)
+                   ntimes = 3, data = prediction_data_fin,
+                   beta_location = "Beta", tol = 0.001, interval = c(0,40000), return_mean_PPC = T,
+                   maximum = T)
 #we take the best lambda and look at the results
 ridge_model = ridge_regression(home_team_win ~ . - id,
                                data = prediction_data_fin, lambda = opt_lam$maximum)
+
+# Comparing Ridge Regression and Logistic Regression ----------------------
 #Now we use crossvalidation to return the optimal  thrashold and associated PCC
 set.seed(123)
 list_cv_err = list()
 list_cv_err$ridge_cv_err = cross_validation(fct = ridge_regression, formula = home_team_win ~ . - id,
-                                ntimes = 4, data = prediction_data_fin, 
-                                beta_location = "Beta", lambda = opt_lam$maximum,return_mean_PPC = F)
+                                            ntimes = 4, data = prediction_data_fin, 
+                                            beta_location = "Beta", lambda = opt_lam$maximum,return_mean_PPC = F)
 #Check the train set AUC
 auc(response = prediction_data_fin$home_team_win, predictor = as.numeric(ridge_model$predictions))
 #We perfrom the cross validation also for 2 of the regressions
@@ -171,9 +183,9 @@ auc(response = prediction_data_fin$home_team_win, predictor = as.numeric(ridge_m
 set.seed(123)
 list_cv_err$reg_no_interact =
   cross_validation(fct = glm, formula = home_team_win ~ . - id,
-                                               ntimes = 3, data = prediction_data_fin,
-                                               beta_location = "coefficients", family = binomial(link = "logit"),
-                                               return_mean_PPC = F)
+                   ntimes = 3, data = prediction_data_fin,
+                   beta_location = "coefficients", family = binomial(link = "logit"),
+                   return_mean_PPC = F)
 list_cv_err$reg_no_interact
 set.seed(123)
 list_cv_err$reg_mean        =
