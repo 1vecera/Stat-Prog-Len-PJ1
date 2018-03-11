903bc93086090b80dc4f7c2b743c6979ba176c7c
jessiehsieh <jessie.hsiehpm@gmail.com>
2018-03-12 04:33:59 +0800

    updated comments

--- Quanlet1 Preprocessing/preprocessing.R
+++ Quanlet1 Preprocessing/preprocessing.R
@@ -1,53 +1,81 @@
-library ( RSQLite )
-library ( dplyr )
-library ( stringr )
-library ( purrr )
+# load required packages
+if (!require("RSQLite")) {
+  install.packages("RSQLite", dependencies = TRUE)
+  library(RSQLite)
+}
+if (!require("dplyr")) {
+  install.packages("dplyr", dependencies = TRUE)
+  library(dplyr)
+}
+if (!require("stringr")) {
+  install.packages("stringr", dependencies = TRUE)
+  library(stringr)
+}
+if (!require("purrr")) {
+  install.packages("purrr", dependencies = TRUE)
+  library(purrr)
+}
 
-con = dbConnect ( SQLite () , dbname =" database . sqlite ")
-dbListTables (con )
+# read the database file and look at all tables contained inside
+con = dbConnect(SQLite(), dbname = "database.sqlite")
+dbListTables(con)
 
-Match = tbl_df ( dbGetQuery (con ," SELECT * FROM Match ") )
-Player = tbl_df ( dbGetQuery (con ," SELECT * FROM Player ") )
-Player_Attributes = tbl_df ( dbGetQuery (con , " SELECT * FROM
-                                            Player_Attributes ") )
+# convert the tables needed into tibbles using simple queries
+Match = tbl_df(dbGetQuery(con, "SELECT * FROM Match"))
+Player = tbl_df(dbGetQuery(con, "SELECT * FROM Player"))
+Player_Attributes = tbl_df(dbGetQuery(con, "SELECT * FROM
+                                            Player_Attributes"))
 
-Player$birthday = as.Date( Player$birthday )
-colnames ( Player_Attributes ) [4] = " date_recorded "
-Player_Attributes$attacking_work_rate = NULL     # Ambiguous levels
-Player_Attributes$defensive_work_rate = NULL     # Ambiguous levels
-Player_Attributes$date_recorded = as.Date(Player_Attributes$date_recorded )
-group1 = Player_Attributes [ unlist ( lapply ( Player_Attributes ,
-                                                  function (X) sum(is.na(X) )== 836) ) ]
-test = subset (group1 , is.na( group1$overall_rating ) )
-summary ( test )
-group2 = Player_Attributes [ unlist ( lapply ( Player_Attributes ,
-                                                  function (X) sum(is.na(X) )== 2713) ) ]
-test = subset (group1 , is.na( group1$vision ) )
-summary ( test )
+# correct data type and make colnames intuitive
+Player$birthday = as.Date(Player$birthday)
+colnames(Player_Attributes)[4] = " date_recorded "
+Player_Attributes$date_recorded = as.Date(Player_Attributes$date_recorded)
 
-Player_Attributes = subset ( Player_Attributes , is.na(
-  Player_Attributes$positioning ) == FALSE )
-Player_Attributes = subset ( Player_Attributes , is.na(
-  Player_Attributes$curve ) == FALSE )
-Player_Attributes$preferred_foot_is_right = as.numeric (as.factor (
-  Player_Attributes$preferred_foot ) ) -1
+Player_Attributes$attacking_work_rate = NULL  # dropped due to ambiguous levels
+Player_Attributes$defensive_work_rate = NULL  # dropped due to ambiguous levels
 
+# check if the player attribute records are consistently missing
+group1 = Player_Attributes[unlist(lapply(Player_Attributes, function(X) sum(is.na(X)) == 836))]
+test = subset(group1, is.na(group1$overall_rating))
+
+group2 = Player_Attributes[unlist(lapply(Player_Attributes, function(X) sum(is.na(X)) == 2713))]
+test = subset(group1, is.na(group1$vision))
+
+# remove records which are problematic as confirmed above
+Player_Attributes = subset(Player_Attributes, is.na(Player_Attributes$positioning) == FALSE)
+Player_Attributes = subset(Player_Attributes, is.na(Player_Attributes$curve) == FALSE)
+
+# make this feature binary
+Player_Attributes$preferred_foot_is_right = 
+                          as.numeric(as.factor(Player_Attributes$preferred_foot)) - 1
 Player_Attributes$preferred_foot = NULL
 
+# use an inner_join to get a dataframe which contains info from both Player and Player_Attributes
 Player_Attributes$id = NULL
-Player_all = inner_join (Player , Player_Attributes )
+Player_all = inner_join(Player, Player_Attributes)
 
+# drop columns which are not needed
 Match$country_id = NULL
 Match$match_api_id = NULL
-colnames ( Match ) [10:53]
-Match [ ,10:53] = NULL
-Match$season = as.factor ( Match$season )
-Match$stage = as.factor ( Match$stage )
-Match$date = as.Date ( Match$date )
+colnames(Match)[10:53]
+Match[, 10:53] = NULL
 
-num_na = function ( column ) { return (sum (is.na( column ) ) ) }
-lapply (Match , num_na )
-avg_num_na = function ( column ) { return (sum(is.na( column ) ) /22) }
-Match [ ,c (3 ,10:31) ] %>% split (. $season ) %>% map( avg_num_na )
+# appropriate data types
+Match$season = as.factor(Match$season)
+Match$stage = as.factor(Match$stage)
+Match$date = as.Date(Match$date)
 
-Match_without0809 = subset (Match , as.character ( season ) !=" 2008/2009") 
+# check number of missing values in all columns, so NAs exist only in positions and betting odds
+num_na = function(column) {
+    return(sum(is.na(column)))
+}
+lapply(Match, num_na)
+
+# check number of average number of missing values of players in each season
+avg_num_na = function(column) {
+    return(sum(is.na(column))/22)
+}
+Match[, c(3, 10:31)] %>% split(.$season) %>% map(avg_num_na)
+
+# get rid of season 2008 to 2009 due to too many NAs
+Match_without0809 = subset(Match, as.character(season) != " 2008/2009")
