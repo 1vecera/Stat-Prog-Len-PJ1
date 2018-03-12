8f93c29bfe8397daefbe77cd227261a66a697891
jessiehsieh <jessie.hsiehpm@gmail.com>
2018-03-12 13:29:24 +0800

    formatting

--- Quanlet1 Preprocessing/preprocessing.R
+++ Quanlet1 Preprocessing/preprocessing.R
@@ -1,24 +1,24 @@
 if(!file.exists("database.sqlite")){
-  print("Please download the database file from kaggle.com and put inside Quanlet 1.")
-  print("https://www.kaggle.com/hugomathien/soccer/data")
+    print("Please download the database file from kaggle.com, unzip and put in working directory.")
+    print("https://www.kaggle.com/hugomathien/soccer/data")
 }
 
 # load required packages
 if (!require("RSQLite")) {
-  install.packages("RSQLite", dependencies = TRUE)
-  library(RSQLite)
+    install.packages("RSQLite", dependencies = TRUE)
+    library(RSQLite)
 }
 if (!require("dplyr")) {
-  install.packages("dplyr", dependencies = TRUE)
-  library(dplyr)
+    install.packages("dplyr", dependencies = TRUE)
+    library(dplyr)
 }
 if (!require("stringr")) {
-  install.packages("stringr", dependencies = TRUE)
-  library(stringr)
+    install.packages("stringr", dependencies = TRUE)
+    library(stringr)
 }
 if (!require("purrr")) {
-  install.packages("purrr", dependencies = TRUE)
-  library(purrr)
+    install.packages("purrr", dependencies = TRUE)
+    library(purrr)
 }
 
 # read the database file and look at all tables contained inside
@@ -26,49 +26,49 @@ con = dbConnect(SQLite(), dbname = "database.sqlite")
 dbListTables(con)
 
 # convert the tables needed into tibbles using simple queries
-Match = tbl_df(dbGetQuery(con, "SELECT * FROM Match"))
-Player = tbl_df(dbGetQuery(con, "SELECT * FROM Player"))
+Match             = tbl_df(dbGetQuery(con, "SELECT * FROM Match"))
+Player            = tbl_df(dbGetQuery(con, "SELECT * FROM Player"))
 Player_Attributes = tbl_df(dbGetQuery(con, "SELECT * FROM
                                             Player_Attributes"))
 
 # correct data type and make colnames intuitive
-Player$birthday = as.Date(Player$birthday)
-colnames(Player_Attributes)[4] = " date_recorded "
+Player$birthday                 = as.Date(Player$birthday)
+colnames(Player_Attributes)[4]  = "date_recorded"
 Player_Attributes$date_recorded = as.Date(Player_Attributes$date_recorded)
 
-Player_Attributes$attacking_work_rate = NULL  # dropped due to ambiguous levels
-Player_Attributes$defensive_work_rate = NULL  # dropped due to ambiguous levels
+# drop columns due to ambiguous levels
+Player_Attributes$attacking_work_rate = NULL  
+Player_Attributes$defensive_work_rate = NULL  
 
-# check if the player attribute records are consistently missing
+# check in summary of test if the player attribute records are consistently missing
 group1 = Player_Attributes[unlist(lapply(Player_Attributes, function(X) sum(is.na(X)) == 836))]
-test = subset(group1, is.na(group1$overall_rating))
+test   = subset(group1, is.na(group1$overall_rating))
 
 group2 = Player_Attributes[unlist(lapply(Player_Attributes, function(X) sum(is.na(X)) == 2713))]
-test = subset(group1, is.na(group1$vision))
+test   = subset(group2, is.na(group2$vision))
 
 # remove records which are problematic as confirmed above
-Player_Attributes = subset(Player_Attributes, is.na(Player_Attributes$positioning) == FALSE)
-Player_Attributes = subset(Player_Attributes, is.na(Player_Attributes$curve) == FALSE)
+Player_Attributes = subset(Player_Attributes, is.na(Player_Attributes$overall_rating) == FALSE)
+Player_Attributes = subset(Player_Attributes, is.na(Player_Attributes$vision)         == FALSE)
 
 # make this feature binary
-Player_Attributes$preferred_foot_is_right = 
-                          as.numeric(as.factor(Player_Attributes$preferred_foot)) - 1
-Player_Attributes$preferred_foot = NULL
+Player_Attributes$preferred_foot_is_right                                              = 0
+Player_Attributes$preferred_foot_is_right[Player_Attributes$preferred_foot == "right"] = 1
+Player_Attributes$preferred_foot                                                       = NULL
 
 # use an inner_join to get a dataframe which contains info from both Player and Player_Attributes
 Player_Attributes$id = NULL
-Player_all = inner_join(Player, Player_Attributes)
+Player_all           = inner_join(Player, Player_Attributes)
 
 # drop columns which are not needed
-Match$country_id = NULL
+Match$country_id   = NULL
 Match$match_api_id = NULL
-colnames(Match)[10:53]
-Match[, 10:53] = NULL
+Match[, 10:53]     = NULL
 
 # appropriate data types
 Match$season = as.factor(Match$season)
-Match$stage = as.factor(Match$stage)
-Match$date = as.Date(Match$date)
+Match$stage  = as.factor(Match$stage)
+Match$date   = as.Date(Match$date)
 
 # check number of missing values in all columns, so NAs exist only in positions and betting odds
 num_na = function(column) {
